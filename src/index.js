4c80219df75abdec5015a1b7ed24aa4d66589e5c
Matt Crowder <matt.crowder@walmart.com>
2018-08-15 11:27:20 -0400

    done!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import * as messages from "./messages";
+// import * as messages from "./messages";
 
-console.log(messages.HELLO);
+// console.log(messages.HELLO);
+
+import { HELLO } from "./messages";
+
+console.log(HELLO);
--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,19 @@
 const path = require("path");
-const webpack = require("webpack");
-const CompressionPlugin = require("compression-webpack-plugin");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
-const isProd = process.env.NODE_ENV === "production";
-const sourcePath = path.join(__dirname, "./src/client");
-const webpackConfig = {
+module.exports = {
   entry: "./src/index.js",
   output: {
-    path: `${__dirname}/build`,
-    filename: "bundle.js"
+    filename: "build/bundle.js"
   },
   resolve: {
-    extensions: [".js", ".scss", ".jsx", ".css"],
-    modules: [sourcePath, path.resolve(__dirname, "./node_modules")],
-    symlinks: true
+    extensions: [".js"]
   },
   module: {
     loaders: [
       {
-        test: /\.js$/,
         include: path.resolve(__dirname, "src"),
-        loader: "babel-loader",
-        exclude: /node_modules/
+        loader: "babel-loader"
       }
     ]
-
-  },
-
+  }
 };
-webpackConfig.plugins = [
-  new webpack.EnvironmentPlugin(["NODE_ENV"]),
-
-  new webpack.DefinePlugin({
-    "process.env.NODE_ENV": "\"production\""
-  }),
-
-];
-if (process.env.ANALYZE_BUNDLE) {
-  webpackConfig.plugins.push(new BundleAnalyzerPlugin());
-}
-if (isProd) {
-
-  webpackConfig.plugins.push(
-    new webpack.NoEmitOnErrorsPlugin(),
-    new CompressionPlugin({
-      asset: "[path].gz[query]",
-      algorithm: "gzip",
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0.8
-    })
-  );
-}
-module.exports = webpackConfig;
